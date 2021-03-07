70e6ff1b8794c40c97350ad4f37aa4a32cc864b7
Shogo Kondo <tip.hp67@gmail.com>
2021-03-07 22:32:41 +0900

    add post function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,62 @@
 class PostsController < ApplicationController
 
+  # 投稿一覧
   def index
-    @posts = Post.all
+    @posts = Post.all.order(created_at: :desc)
   end
 
+  # 投稿詳細
+  def show
+    @post = Post.find_by(id: params[:id])
+  end
+
+  # 新規投稿
   def new
+    @post = Post.new
   end
 
+  def create
+    @post = Post.new(title: params[:title], author: params[:author])
+    if @post.save
+      # 保存成功
+      redirect_to("/posts/index")
+    else
+      # 保存失敗
+      render("posts/new")
+    end
+  end
+
+  # 編集
   def edit
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def update
+    @post = Post.find_by(id: params[:id])
+    @post.title = params[:title]
+    @post.author = params[:author]
+    @post.content = params[:content]
+    
+    if @post.save
+      # 保存成功
+      redirect_to("/posts/index")
+    else
+      # 保存失敗
+      render("posts/edit")
+    end
+  end
+
+  # 削除
+  def destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
     redirect_to("/posts/index")
   end
+
+  def fav
+  end
+
+  def ranking
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,26 @@
 Rails.application.routes.draw do
 
+  # home controller
   get "/" => "home#top"
   get "about" => "home#about"
 
-  get 'user/index'
+  # users controller
+  get "users/index" => "users#index"
   get "signup" => "users#new"
   post "users/create" => "users#create"
   get "login" => "users#login_form"
   post "login" => "users#login"
 
-
+  # posts controller
   get "posts/index" => "posts#index"
   get "posts/new" => "posts#new"
-  post "posts/edit" => "posts#edit"
+  post "posts/create" => "posts#create"
+  get "posts/:id" => "posts#show"
+  get "posts/:id/edit" => "posts#edit"
+  post "posts/:id/update" => "posts#update"
+  post "posts/:id/destroy" => "posts#destroy"
 
+  get "posts/fav" => "posts#fav"
+  get "ranking" => "posts#ranking"
 
 end
